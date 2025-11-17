fc532875513d987f337412b51bad22f3511a2f1f
Issam-BH <issambh44@gmail.com>
2025-11-17 16:16:19 +0100

    PO + Admin test

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[IsGranted('ROLE_ADMIN')]
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(Request $request, Connection $connection): Response
+    public function index(Request $request, Connection $connection,UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $message = null;
 
@@ -32,13 +34,17 @@ final class AdminController extends AbstractController
 
         if ($request->isMethod('POST')) {
 
-            $clientName = $request->request->get('client_name');
-            $email = $request->request->get('email');
+            $user = new User();
+            $user->setPassword
+
+            $username = $request->request->get('username');
+            $password = $request->request->get('password');
             $requestId = $request->request->get('request_id');
 
-            $connection->insert('clients', [
-                'client_name' => $clientName,
-                'email' => $email
+            $connection->insert('user', [
+                'username' => $username,
+                'password' => $userPasswordHasher->hashPassword($username, $password),
+                'roles' => '["ROLE_COMMERCANT"]'
             ]);
 
             $connection->update('po_requests', [
--- src/Controller/ProductOwnerController.php
+++ src/Controller/ProductOwnerController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\CompteClient;
+use App\Entity\PoRequestAccount;
+use App\Config\PoAccountRequestAction;
+use App\Config\PoAccountRequestStatus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -20,6 +24,35 @@ final class ProductOwnerController extends AbstractController
         return $this->render('product_owner/index.html.twig', [
             'controller_name' => 'ProductOwnerController',
             'comptes' => $comptes
+        ]);
+    }
+
+        #[Route('/product-owner/dashboard', name: 'app_product_owner_dashboard')]
+    public function dashboard(EntityManagerInterface $entityManager,Request $request): Response
+    {
+        $comptes = $entityManager->getRepository(CompteClient::class)->findAll();
+        $message = null;
+
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('username');
+            $password = $request->request->get('password');
+            $action = $request->request->get('action');
+
+            $poRequest = new PoRequestAccount();
+            $poRequest->setUsername($username)
+                      ->setPassword($password)
+                      ->setAction(PoAccountRequestAction::from($action))
+                      ->setStatus(PoAccountRequestStatus::pending);
+
+            $entityManager->persist($poRequest);
+            $entityManager->flush();
+
+            $message = "✅ La demande a été envoyée avec succès.";
+        }
+
+        return $this->render('product_owner/dashboard.html.twig', [
+            'comptes' => $comptes,
+            'message' => $message,
         ]);
     }
 }
